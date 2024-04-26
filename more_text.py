ca16f51a1b2df7e3f337d1b9659b9360f66e3752
Chutipon Trirattananurak <166491120+ChutiponTri@users.noreply.github.com>
2024-04-26 14:26:14 +0700

    Update more_text.py

--- more_text.py
+++ more_text.py
@@ -16,20 +16,5 @@ class Stream():
         # Caption
         st.caption("Hi I am Caption")
 
-        # Latex
-        st.latex(r"\begin{pmatrix} a&b\\c&d \end{pmatrix}")
-        st.markdown("### [Latex (Format)](https://katex.org/docs/supported.html)")
-
-        # Json
-        json = {"a":"1,2,3", "b":"4,5,6"}
-        st.json(json)
-
-        # Code
-        code = '''
-        print("Hello World")
-        def function_A():
-            return "Me"'''
-        st.code(code, language="python")
-
 if __name__ == '__main__':
     stream = Stream()
