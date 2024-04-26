78751543076eb7121e66062959283687210fa256
Chutipon Trirattananurak <166491120+ChutiponTri@users.noreply.github.com>
2024-04-26 14:32:54 +0700

    Update more_text.py

--- more_text.py
+++ more_text.py
@@ -3,18 +3,7 @@ import streamlit as st
 class Stream():
     def __init__(self):
         # Title
-        st.title("Title Dummy")
-        st.header("Header Dummy")
-        st.subheader("Subheader for me")
-        st.text("My name is YES Wheelchair, I was born 19th March 2011")
-
-        # Markdown
-        st.markdown("># Hello *World*")
-        st.markdown("---")
-        st.markdown("### [Google](https://www.google.com)")
-
-        # Caption
-        st.caption("Hi I am Caption")
+        st.write("Hello World")
 
 if __name__ == '__main__':
     stream = Stream()
