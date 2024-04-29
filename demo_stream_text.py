52257fd9a76d091e4e0f8c79dd106b315d7f2b97
Chutipon Trirattananurak <166491120+ChutiponTri@users.noreply.github.com>
2024-04-29 22:40:37 +0700

    Add files via upload

---
+++ demo_stream_text.py
@@ -0,0 +1,46 @@
+text = '''ALL Wheelchair: An Interdisciplinary Project by the Faculty of Engineering and the Faculty of Allied Health Sciences
+
+Abstract:
+
+ALL Wheelchair project stands as a testament to the collaborative efforts between the esteemed Faculty of Engineering and the innovative Faculty of Allied Health Sciences. Designed to enhance mobility and independence for individuals with diverse physical abilities, this interdisciplinary endeavor marries cutting-edge engineering principles with advanced healthcare insights.
+
+Introduction:
+
+In response to the pressing need for inclusive mobility solutions, the Faculty of Engineering and the Faculty of Allied Health Sciences joined forces to conceive ALL Wheelchair project. Recognizing the pivotal role mobility plays in individuals' quality of life, this collaboration endeavors to revolutionize traditional ALL Wheelchair C design and functionality.
+
+Engineering Innovations:
+
+The engineering facet of the project delves deep into the realm of material sciences, biomechanics, and human-centered design. Engineers leverage state-of-the-art technologies such as 3D printing, lightweight alloys, and smart sensors to craft wheelchairs that prioritize durability, maneuverability, and user comfort. Furthermore, advancements in battery technology and motor systems ensure efficient propulsion and extended operational lifespans.
+
+Healthcare Integration:
+
+Complementing the engineering ingenuity is the invaluable contribution from the Faculty of Allied Health Sciences. Healthcare professionals lend their expertise in ergonomic assessment, rehabilitation therapy, and user interface design. By understanding users' unique physiological needs and functional limitations, healthcare experts tailor ALL Wheel C features to optimize posture support, pressure distribution, and ease of use. Additionally, ongoing research fosters continuous improvements, ensuring that ALL Wheelchair project remains at the forefront of assistive technology.
+
+Community Impact:
+
+Beyond the confines of academia, ALL Wheelchair project extends its reach to empower communities and foster inclusivity. Collaborations with local rehabilitation centers, disability advocacy groups, and governmental agencies facilitate the integration of ALL Wheel C prototypes into real-world settings. By soliciting feedback from end-users and caregivers, the project remains rooted in user-centered design principles, ensuring that every iteration addresses genuine needs and enhances user autonomy.
+
+Conclusion:
+
+ALL Wheelchair project epitomizes the transformative potential of interdisciplinary collaboration. By harnessing the collective expertise of engineers and healthcare professionals, this endeavor transcends conventional boundaries to create impactful solutions that empower individuals with disabilities. As the project continues to evolve, it not only revolutionizes assistive technology but also inspires future generations to embrace the ethos of inclusivity and innovation.'''
+
+page_background = """
+<style>
+[data-testid="stAppViewContainer"] >.main{
+background-image: url("https://raw.githubusercontent.com/chutipon007/LE202/main/test_bg2.jpg");
+background-size: cover;
+# background-position: top left;
+# background-attachment: local;
+}
+
+[data-testid="stHeader"]{
+background-color: rgba(0, 0, 0 , 0);
+}
+
+[data-testid="stSidebar"]{
+background-image: url("https://raw.githubusercontent.com/chutipon007/LE202/main/test_sidebar.jpg");
+background-size: cover;
+background-position: right;
+}
+</style>
+"""
