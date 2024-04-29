e3515bc7b16128dd5b19d4b6e52eb1c9add0c4d6
Chutipon Trirattananurak <166491120+ChutiponTri@users.noreply.github.com>
2024-04-29 17:23:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,29 @@
 import streamlit as st
+import firebase_admin
+from firebase_admin import credentials, db
 
-class Stream():
-    def __init__(self):
-        st.write("Hello World")
+def get_database(username):
+    if not firebase_admin._apps:
+        # Initialize Firebase Admin SDK with service account credentials
+        cred = credentials.Certificate("database/allwheelchair-firebase-adminsdk-rjisr-e43646aa27.json")
+        firebase_admin.initialize_app(cred, {
+            'databaseURL': 'https://allwheelchair-default-rtdb.asia-southeast1.firebasedatabase.app/'
+        })
+
+    # Get a reference to the Firebase Realtime Database
+    ref = db.reference('/')
+
+    data = ref.child("users/%s" % username).get()
+
+    return data
+
+def stream(data):
+    st.write("Hello World")
+    # st.write(data)
+    for json in data:
+        st.write(json)
+        st.dataframe(data[json])
 
 if __name__ == '__main__':
-    stream = Stream()
+    data = get_database("Ton")
+    stream(data)
